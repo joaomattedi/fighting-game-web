460b047ec0d093c0c62d11aedb39106bd29afd01
joaomattedi <joaommattedi@gmail.com>
2022-05-02 12:52:47 -0300

    create canvas and setting up size

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+context.fillRect(0,0,canvas.width,canvas.height);
