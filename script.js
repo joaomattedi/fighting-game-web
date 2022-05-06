9a9694ed2d86e19ddcf77220e3e2c5f6a0d88358
joaomattedi <joaommattedi@gmail.com>
2022-05-06 11:05:52 -0300

    collision in y axis conflict solved, attackbox must be between enemy position y and enemy height

--- script.js
+++ script.js
@@ -115,8 +115,10 @@ function animate() {
   }
 
   //detect for collision
-  if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x && player.attackBox.position.x <= enemy.position.x + enemy.width && player.attackBox.position.y + 
-    player.attackBox.height >= enemy.position.y) {
+  if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x 
+    && player.attackBox.position.x <= enemy.position.x + enemy.width && 
+    player.attackBox.position.y + player.attackBox.height >= enemy.position.y
+    && player.attackBox.position.y <= enemy.position.y + enemy.height) {
     console.log('contact');
   }
 }
